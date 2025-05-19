5da38f1a7ae6588d97aaec3312c2577c45b8cadf
ValeriiaZhubrieva <67803539+ValeriiaZhubrieva@users.noreply.github.com>
2025-05-19 18:18:58 +0300

    fix v1

--- js/dev/app.js
+++ js/dev/app.js
@@ -50,3 +50,18 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     }
   });
 });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - 20,
+        // Сдвигаем на высоту заголовка, если нужно
+        behavior: "smooth"
+        // Плавный скролл
+      });
+    }
+  });
+});
